c15a2ecdf8c61f55c79b44cad8b8227b23086054
pradeepInnovex <pradeepinnovax@gmail.com>
2022-12-16 05:48:59 +0530

    Demo Calculator

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-print('Hello', 'World', sep=",", end=",")
-print('India', 'Brasil', sep=",")
+print("This is Demo Calculator for Add Two Numbers:")
+print("Enter First Number :", end=" ")
+x = int(input())
+
+print("Enter Second Number :", end=" ")
+y = int(input())
+
+result = x+y
+print("Your Result is :", result)
