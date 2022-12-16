846241114802368e13d0fd29a36d83182fca2e69
pradeepInnovex <pradeepinnovax@gmail.com>
2022-12-17 04:07:57 +0530

    Leaf Year Example

--- test.py
+++ test.py
@@ -1,11 +1,26 @@
-print('Write a Program to print even number in 1 to 10')
+"""
+Leaf Year Conditions :
+01. Year Divide by 400
+02. Year cannot Divide by 100 and divide by 4
+"""
+print('Enter your year :')
 
-for num in range(1, 11):
+year = int(input())
+status = 'null'
 
-    if num % 2 == 0:
-        print(num)
-    else:
-        print('')
+if year % 400 == 0:
+    status = 'true'
+elif year % 100 > 0 and year % 4 == 0:
+    status = 'true'
+else:
+    status = 'false'
+
+if status == 'true':
+    print('Leaf Year')
+else:
+    print('Not Leaf Year')
+
+
 
 
 
