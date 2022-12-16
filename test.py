dfdda662174f4d95e469ab097922690f20d705fc
pradeepInnovex <pradeepinnovax@gmail.com>
2022-12-17 04:19:29 +0530

    Multiplication Table

--- test.py
+++ test.py
@@ -1,26 +1,3 @@
-"""
-Leaf Year Conditions :
-01. Year Divide by 400
-02. Year cannot Divide by 100 and divide by 4
-"""
-print('Enter your year :')
 
-year = int(input())
-status = 'null'
-
-if year % 400 == 0:
-    status = 'true'
-elif year % 100 > 0 and year % 4 == 0:
-    status = 'true'
-else:
-    status = 'false'
-
-if status == 'true':
-    print('Leaf Year')
-else:
-    print('Not Leaf Year')
-
-
-
-
-
+for i in range(1, 13):
+    print(i, 'x', 3, '=', i*3)
